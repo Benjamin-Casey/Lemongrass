f03ba0dfe47289d441c5ad183d02ef7d57728151
Benjamin Casey <benjamincasey55@gmail.com>
2020-06-21 19:45:28 +1000

    Added a database context class and attempted to add a listing to it. Not working because the Listing object does not have a unique ID (key).

--- MarketSimulation/MarketSimulation/Program.cs
+++ MarketSimulation/MarketSimulation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace MarketSimulation
 {
@@ -20,6 +21,35 @@ namespace MarketSimulation
 			Market someTown = new Market("The Town of Some");
 			someTown.AddListing(playerBuyListing);
 
+			using (var db = new MarketListingContext())
+			{
+				// Create and save new listing
+				// Note: This adds one fixed listing. To make this more realistic
+				//		 it should ask for user input to create different listings
+				//		 each time.
+				var listing = new Listing(Lemon, 1, Apple, 1);
+				db.MarketListings.Add(listing);
+				db.SaveChanges();
+
+				// Display all listings from the db
+				var query = from l in db.MarketListings
+							orderby l.ItemToSell.name
+							select l;
+
+				// All listings in the db:
+				foreach(var item in query)
+				{
+					Console.WriteLine(item.ItemToSell.name);
+				}
+
+				Console.ReadKey();
+			}
+
         }
+
+		public class MarketListingContext : DbContext
+		{
+			public DbSet<Listing> MarketListings { get; set; }
+		}
 	}
 }
